ea9eacc8c5272fbeb977175db48e0822c8401af1
Santiago Salazar <santiago1234ocampo@gmail.com>
2022-11-03 16:41:16 -0300

    Navbar V0 terminado

---
+++ portafolio/src/Nav.js
@@ -0,0 +1,14 @@
+import "./nav.module.css";
+
+export default function Nav() {
+    return (
+        <header>
+            <nav>
+                <div><a href='/'>Inicio</a></div>
+                <div><a href='/'>Sobre mí</a></div>
+                <div><a href='/'>Proyectos</a></div>
+                <div><a href='/'>Contacto</a></div>
+            </nav>
+        </header>
+    )
+}
