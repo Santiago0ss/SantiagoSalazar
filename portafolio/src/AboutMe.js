fcc9f4c5388f8801dd326123cb80b08dca51026d
Santiago Salazar <santiago1234ocampo@gmail.com>
2022-11-30 21:36:22 -0300

    imagen de fondo

--- portafolio/src/AboutMe.js
+++ portafolio/src/AboutMe.js
@@ -3,9 +3,6 @@ import styles from './AboutMe.module.css';
 export default function AboutMe() {
     return (
         <section className={styles.aboutMe}>
-            <div id={styles.snow} className={styles.little}></div>
-            <div id={styles.snow} className={styles.mid}></div>
-            <div id={styles.snow} className={styles.big}></div>
             <main className={styles.aboutMeContainer}>
                 <h3>SOBRE MÍ</h3>
                 <p>Hola, soy Santiago</p>
